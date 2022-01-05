4932f9c8a7c74ec15ac5b9715f2435ac12cdc24a
sukhi <sukhikushwaha@gmail.com>
2022-01-05 18:31:21 +0530

    updates

--- emailTracker.js
+++ emailTracker.js
@@ -15,7 +15,6 @@ try{
     if (window.location.search != ''){
         var data = getQueryParams(window.location.search)
         const xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === this.DONE) {
             console.log(this.responseText);
