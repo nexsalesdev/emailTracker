ea2aadf6739236ac43266b4bb84afdb446fcf1ab
sukhi <sukhikushwaha@gmail.com>
2022-06-23 17:00:17 +0530

    mouseMove fix v3

--- clickedIn.js
+++ clickedIn.js
@@ -16,6 +16,20 @@ function getDateTime() {
     return (date.getUTCFullYear() +"-"+ (date.getUTCMonth()+1) +"-"+ date.getUTCDate() +" "+ date.getUTCHours() +":"+ date.getUTCMinutes() +":"+ date.getUTCSeconds());
 }
 
+
+function bindEvents(){
+    document.addEventListener('mousemove',sendDate);
+    document.addEventListener('touchmove',sendDate);
+    document.addEventListener('click',sendDate);
+};
+
+function removeEvents(){
+    document.removeEventListener('mousemove',sendDate);
+    document.removeEventListener('touchmove',sendDate);
+    document.removeEventListener('click',sendDate);
+    console.error('Binding removed');
+};
+
 var myCounter = 0;
 var sendDate = function(){
     try{
@@ -24,7 +38,7 @@ var sendDate = function(){
             var data = getQueryParams(window.location.search);
             data["created_at"] = getDateTime();
             //data["isFrame"] = ((window.self === window.top) ? 'Main window' : 'Frame window');
-            data["isFrame"] = 'MouseTrigerCode v2'
+            data["isFrame"] = 'MouseTrigerCode v3'
             const xhr = new XMLHttpRequest();
             xhr.addEventListener("readystatechange", function () {
                 if (this.readyState === this.DONE) {
@@ -35,18 +49,17 @@ var sendDate = function(){
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.send(JSON.stringify(data));
             console.log('Sent');
-            document.removeEventListener('mousemove',sendDate);
         }else{
-            console.error('No query params found & binding removed');
-            document.removeEventListener('mousemove',sendDate);
+            console.error('No query params found');
         }
+        removeEvents();
     }catch(error) {
         console.error('Could not not send request', error);
     }
 }
 
 try{
-    document.addEventListener('mousemove',sendDate);
+    bindEvents();
 }catch(error) {
     console.error('Could not not send request', error);
 }
